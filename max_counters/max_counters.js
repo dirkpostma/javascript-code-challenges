2c99a99125878ecf3fd5ba46207a86673a9a4601
Dirk Postma <dirk.postma@weareyou.com>
2021-11-19 12:03:33 +0100

    Optimize performance

--- max_counters/max_counters.js
+++ max_counters/max_counters.js
@@ -1,17 +1,22 @@
 const solution = (N, A) => {
   const counters = new Array(N).fill(0);
   let max = 0;
+  let last_fill_max = 0;
 
   for (let i = 0; i < A.length; i++) {
     if (A[i] === N + 1) {
-      counters.fill(max);
+      last_fill_max = max;
     } else {
-      counters[A[i] - 1] += 1;
+      if (counters[A[i] - 1] < last_fill_max) {
+        counters[A[i] - 1] = last_fill_max + 1;
+      } else {
+        counters[A[i] - 1] += 1;
+      }
       max = Math.max(max, counters[A[i] - 1]);
     }
   }
 
-  return counters;
+  return counters.map((value) => Math.max(value, last_fill_max));
 };
 
 module.exports = {
